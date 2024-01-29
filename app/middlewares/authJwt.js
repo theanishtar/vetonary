31d3735060c6b818a8e6c968105e539a7188b5a9
Trần Hữu Đang <89396036+theanishtar@users.noreply.github.com>
2024-01-29 09:20:18 +0700

    Update authJwt.js

--- app/middlewares/authJwt.js
+++ app/middlewares/authJwt.js
@@ -24,71 +24,8 @@ verifyToken = (req, res, next) => {
     });
 };
 
-isAdmin = (req, res, next) => {
-  User.findById(req.userId).exec((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
-
-    // Role.find(
-    //   {
-    //     _id: { $in: user.roles },
-    //   },
-    //   (err, roles) => {
-    //     if (err) {
-    //       res.status(500).send({ message: err });
-    //       return;
-    //     }
-
-    //     for (let i = 0; i < roles.length; i++) {
-    //       if (roles[i].name === "admin") {
-    //         next();
-    //         return;
-    //       }
-    //     }
-
-    //     res.status(403).send({ message: "Require Admin Role!" });
-    //     return;
-    //   }
-    // );
-  });
-};
-
-isModerator = (req, res, next) => {
-  User.findById(req.userId).exec((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
-
-    // Role.find(
-    //   {
-    //     _id: { $in: user.roles },
-    //   },
-    //   (err, roles) => {
-    //     if (err) {
-    //       res.status(500).send({ message: err });
-    //       return;
-    //     }
-
-    //     for (let i = 0; i < roles.length; i++) {
-    //       if (roles[i].name === "moderator") {
-    //         next();
-    //         return;
-    //       }
-    //     }
-
-    //     res.status(403).send({ message: "Require Moderator Role!" });
-    //     return;
-    //   }
-    // );
-  });
-};
 
 const authJwt = {
   verifyToken,
-  isAdmin,
-  isModerator,
 };
 module.exports = authJwt;
