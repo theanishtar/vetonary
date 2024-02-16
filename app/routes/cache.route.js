e33e7f535b8be5feb3c1550e6224cd6d6cf0c009
theanishtar <dangtt135@gmail.com>
2024-02-16 08:55:49 +0700

    feat: authen role isModerator with cache APIs
    Implement authentication for 'isModerator' role with caching for APIs
    
    - Add authentication logic specifically for users with the 'isModerator' role
    - Implement caching mechanism for API endpoints accessed by 'isModerator' users to enhance performance
    - Ensure that 'isModerator' users can securely access authorized APIs with proper authentication checks

--- app/routes/cache.route.js
+++ app/routes/cache.route.js
@@ -1,5 +1,5 @@
 const cache = require("../controllers/redis.controller");
-const auth = require("../middlewares/authMongo");
+const auth = require("../middlewares/authJwt");
 
 module.exports = function (app, redis) {
   app.use(function (req, res, next) {
@@ -11,11 +11,11 @@ module.exports = function (app, redis) {
     next();
   });
 
-  app.get('/api/caches', auth.verifyToken, (req, res) => cache.getAllCache(req, res, redis));
-  app.get('/api/missing', auth.verifyToken, (req, res) => cache.missingRedis(req, res, redis));
-  app.post('/api/caches', auth.verifyToken, (req, res) => cache.addAllMongoToRedis(req, res, redis));
+  app.get('/api/caches', auth.isModerator, (req, res) => cache.getAllCache(req, res, redis));
+  app.get('/api/missing', auth.isModerator, (req, res) => cache.missingRedis(req, res, redis));
+  app.post('/api/caches', auth.isModerator, (req, res) => cache.addAllMongoToRedis(req, res, redis));
   app.get('/api/cache', (req, res) => cache.getCacheByKey(req, res, redis)); //api/cache?key=cút
-  app.delete('/api/cache', auth.verifyToken, (req, res) => cache.deleteByKey(req, res, redis)); //api/cache?key=cút
-  app.put('/api/cache', auth.verifyToken, (req, res) => cache.updateByKey(req, res, redis)); //api/cache?key=cút
-  app.post('/api/cache', auth.verifyToken, (req, res) => cache.postCache(req, res, redis)); //api/cache
+  app.delete('/api/cache', auth.isModerator, (req, res) => cache.deleteByKey(req, res, redis)); //api/cache?key=cút
+  app.put('/api/cache', auth.isModerator, (req, res) => cache.updateByKey(req, res, redis)); //api/cache?key=cút
+  app.post('/api/cache', auth.isModerator, (req, res) => cache.postCache(req, res, redis)); //api/cache
 };
