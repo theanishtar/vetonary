0cdd00e390686c9b87124b6d4b36d0809e7c6980
theanishtar <dangtt135@gmail.com>
2024-04-01 10:27:02 +0700

    feat: add route to index.js

--- server/app/routes/guest.route.js
+++ server/app/routes/guest.route.js
@@ -1,1 +1,13 @@
+const controller = require("../controllers/guest.controller");
 
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post("/api/guest/push", controller.push);
+};
